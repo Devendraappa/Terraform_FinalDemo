9a8f41d057fac9855b53b34cb4d34c9075322218
Devendraappa <devendraappa.kune@gmail.com>
2024-12-29 19:12:13 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "${var.bucket_name_prefix}-${random_id.id.hex}"
+  acl    = "private"
+}
+
+resource "random_id" "id" {
+  byte_length = 8
+}
