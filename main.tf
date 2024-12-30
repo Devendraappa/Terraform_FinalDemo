1389e80670d91bc7997c523daa67a56c2e36e113
Devendraappa <devendraappa.kune@gmail.com>
2024-12-30 18:45:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "example" {
   bucket = "${var.bucket_name_prefix}-${random_id.id.hex}"
-  acl    = "private"
+  aws_s3_bucket_acl = "private"
 }
 
 resource "random_id" "id" {
